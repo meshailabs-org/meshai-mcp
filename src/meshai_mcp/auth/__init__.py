52bc4757f2bd59dd252ad81496732200a97e190d
meshailabs <devsupport@meshai.dev>
2025-08-19 14:12:18 -0400

    Fix auth middleware import error

--- src/meshai_mcp/auth/__init__.py
+++ src/meshai_mcp/auth/__init__.py
@@ -2,23 +2,14 @@
 
 from .client import AuthClient
 from .models import TokenValidation, UserContext, AuthError
-
-# Import middleware only when fastapi is available
-try:
-    from .middleware import AuthMiddleware, get_current_user, get_current_user_optional
-    _MIDDLEWARE_AVAILABLE = True
-except ImportError:
-    AuthMiddleware = None
-    get_current_user = None
-    get_current_user_optional = None
-    _MIDDLEWARE_AVAILABLE = False
+from .middleware import AuthMiddleware, get_current_user, get_current_user_optional
 
 __all__ = [
     'AuthClient',
     'TokenValidation',
     'UserContext', 
-    'AuthError'
+    'AuthError',
+    'AuthMiddleware',
+    'get_current_user',
+    'get_current_user_optional'
 ]
-
-if _MIDDLEWARE_AVAILABLE:
-    __all__.extend(['AuthMiddleware', 'get_current_user', 'get_current_user_optional'])
